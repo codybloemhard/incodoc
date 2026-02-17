77b5fce3aec16bbf0171be5de1eb48bfa665f5e0
Cody Bloemhard <codybloemhard@gmail.com>
2026-02-17 21:04:34 +0100

    maintenance: pest dependency version bump; version: 0.6.1;

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,9 +1,11 @@
+#[allow(unused_macros)]
 macro_rules! props {
     ($slice:expr) => {
         HashMap::from($slice)
     }
 }
 
+#[allow(unused_macros)]
 macro_rules! hset {
     ($slice:expr) => {
         HashSet::from_iter($slice.iter().map(|s| s.to_string()))
